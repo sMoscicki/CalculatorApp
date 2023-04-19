a98ebdbfb1a78346f1a90f9f809008d08949be66
sMoscicki <moscicki.szymonm@gmail.com>
2023-04-19 21:37:09 +0200

    Initial Commit

---
+++ CalculatorApp/CalculatorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorAppApp.swift
+//  CalculatorApp
+//
+//  Created by s.Moscicki on 19/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
